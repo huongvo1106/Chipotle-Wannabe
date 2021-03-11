cf47976dec3f5440f6f9361e07d4728b4cc00206
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2021-03-11 08:16:34 -0600

    Delete dbSetup.java

--- dbSetup.java
+++
@@ -1,9 +0,0 @@
-/*
-CSCE 315
-Database setup example
-Purpose: Keeping usernames and password private.
-*/
-public final class dbSetup  {
-  public static final String user = "huongvo1106";
-  public static final String pswd = "Student0611";
-}//end class
